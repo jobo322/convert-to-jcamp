a18fdc10930555ed1a623d4bf77b74b8169869b3
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2018-02-08 06:04:09 -0500

    chore: update dependencies

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,9 +1,10 @@
+import { convert } from 'jcampconverter';
+
 import convertToJcamp from '..';
-import {convert} from 'jcampconverter';
 
 describe('convertToJcamp', () => {
-    it('check valid with jcampconverter', () => {
-        const testData = `1 2
+  it('check valid with jcampconverter', () => {
+    const testData = `1 2
 2 3
 3 4
 4 5
@@ -11,51 +12,51 @@ describe('convertToJcamp', () => {
 6 7
 7 8
 8 9`;
-        var options = {
-            title: 'test',
-            owner: 'cheminfo',
-            origin: 'manually',
-            type: 'MASS SPECTRUM',
-            xUnit: 'M/Z',
-            yUnit: 'relative abundance',
-            info: {
-                info1: 'value1',
-                info2: 'value2'
-            }
-        };
-        var jcamp = convertToJcamp(testData, options);
-        var jcampObject = convert(jcamp);
+    var options = {
+      title: 'test',
+      owner: 'cheminfo',
+      origin: 'manually',
+      type: 'MASS SPECTRUM',
+      xUnit: 'M/Z',
+      yUnit: 'relative abundance',
+      info: {
+        info1: 'value1',
+        info2: 'value2'
+      }
+    };
+    var jcamp = convertToJcamp(testData, options);
+    var jcampObject = convert(jcamp);
 
-        expect(jcampObject.spectra).toEqual([
-            {
-                data: [[
-                    1, 2,
-                    2, 3,
-                    3, 4,
-                    4, 5,
-                    5, 6,
-                    6, 7,
-                    7, 8,
-                    8, 9
-                ]],
-                dataType: 'MASS SPECTRUM',
-                firstX: 1,
-                firstY: 2,
-                isPeaktable: true,
-                lastX: 8,
-                lastY: 9,
-                nbPoints: 8,
-                title: 'test',
-                xFactor: 1,
-                xUnit: 'M/Z',
-                yFactor: 1,
-                yUnit: 'relative abundance'
-            }
-        ]);
-    });
+    expect(jcampObject.spectra).toEqual([
+      {
+        data: [[
+          1, 2,
+          2, 3,
+          3, 4,
+          4, 5,
+          5, 6,
+          6, 7,
+          7, 8,
+          8, 9
+        ]],
+        dataType: 'MASS SPECTRUM',
+        firstX: 1,
+        firstY: 2,
+        isPeaktable: true,
+        lastX: 8,
+        lastY: 9,
+        nbPoints: 8,
+        title: 'test',
+        xFactor: 1,
+        xUnit: 'M/Z',
+        yFactor: 1,
+        yUnit: 'relative abundance'
+      }
+    ]);
+  });
 
-    it('check with default values', () => {
-        const testData = `2 3
+  it('check with default values', () => {
+    const testData = `2 3
 1 2
 3 4
 -4 5
@@ -64,31 +65,31 @@ describe('convertToJcamp', () => {
 7 8
 8 9`;
 
-        var jcamp = convertToJcamp(testData);
-        var jcampObject = convert(jcamp);
+    var jcamp = convertToJcamp(testData);
+    var jcampObject = convert(jcamp);
 
-        expect(jcampObject.spectra).toEqual([{
-            data: [[
-                2, 3,
-                1, 2,
-                3, 4,
-                5, 16,
-                6, 7,
-                7, 8,
-                8, 9
-            ]],
-            dataType: '',
-            firstX: 1,
-            firstY: 2,
-            isPeaktable: true,
-            lastX: 8,
-            lastY: 16,
-            nbPoints: 7,
-            title: '',
-            xFactor: 1,
-            xUnit: '',
-            yFactor: 1,
-            yUnit: ''
-        }]);
-    });
+    expect(jcampObject.spectra).toEqual([{
+      data: [[
+        2, 3,
+        1, 2,
+        3, 4,
+        5, 16,
+        6, 7,
+        7, 8,
+        8, 9
+      ]],
+      dataType: '',
+      firstX: 1,
+      firstY: 2,
+      isPeaktable: true,
+      lastX: 8,
+      lastY: 16,
+      nbPoints: 7,
+      title: '',
+      xFactor: 1,
+      xUnit: '',
+      yFactor: 1,
+      yUnit: ''
+    }]);
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {parse as parseXY} from 'xy-parser';
+import { parse as parseXY } from 'xy-parser';
 
 /**
  * Convert strings into JCAMP and add extra information
@@ -14,46 +14,46 @@ import {parse as parseXY} from 'xy-parser';
  * @param {object} [options.parser = {}] - 'xy-parser' options. arrayType = 'xyxy' is enforced
  * @return {string} JCAMP of the input
  */
-export default function (data, options = {}) {
-    const {
-        title = '',
-        owner = '',
-        origin = '',
-        type = '',
-        xUnit = '',
-        yUnit = '',
-        info = {},
-        parser = {}
-    } = options;
+export default function convertToJcamp(data, options = {}) {
+  const {
+    title = '',
+    owner = '',
+    origin = '',
+    type = '',
+    xUnit = '',
+    yUnit = '',
+    info = {},
+    parser = {}
+  } = options;
 
-    parser.arrayType = 'xyxy';
-    var lines = parseXY(data, parser);
-    var firstX, lastX, firstY, lastY;
-    var points = [];
+  parser.arrayType = 'xyxy';
+  var lines = parseXY(data, parser);
+  var firstX, lastX, firstY, lastY;
+  var points = [];
 
-    for (var i = 0; i < lines.length; i++) {
-        if (lines[i].length > 1) {
-            var x = Number(lines[i][0]);
-            var y = Number(lines[i][1]);
-            if ((!firstX) || ((firstX > x) && (x > 0))) {
-                firstX = x;
-            }
-            if ((!lastX) || (lastX < x)) {
-                lastX = x;
-            }
-            if ((!firstY) || (firstY > y)) {
-                firstY = y;
-            }
-            if ((!lastY) || (lastY < y)) {
-                lastY = y;
-            }
-        }
-        if (x > 0) {
-            points.push(x + '\t' + y);
-        }
+  for (var i = 0; i < lines.length; i++) {
+    if (lines[i].length > 1) {
+      var x = Number(lines[i][0]);
+      var y = Number(lines[i][1]);
+      if ((!firstX) || ((firstX > x) && (x > 0))) {
+        firstX = x;
+      }
+      if ((!lastX) || (lastX < x)) {
+        lastX = x;
+      }
+      if ((!firstY) || (firstY > y)) {
+        firstY = y;
+      }
+      if ((!lastY) || (lastY < y)) {
+        lastY = y;
+      }
     }
+    if (x > 0) {
+      points.push(`${x}\t${y}`);
+    }
+  }
 
-    var header = `##TITLE=${title}
+  var header = `##TITLE=${title}
 ##JCAMP-DX=4.24
 ##DATA TYPE=${type}
 ##ORIGIN=${origin}
@@ -65,12 +65,12 @@ export default function (data, options = {}) {
 ##FIRSTY=${firstY}
 ##LASTY=${lastY}\r\n`;
 
-    for (const key of Object.keys(info)) {
-        header += `##$${key}=${info[key]}\r\n`;
-    }
+  for (const key of Object.keys(info)) {
+    header += `##$${key}=${info[key]}\r\n`;
+  }
 
-    header += `##NPOINTS=${points.length}
+  header += `##NPOINTS=${points.length}
 ##PEAK TABLE=(XY..XY)\r\n`;
 
-    return header.replace(/[^\t\r\n\x20-\x7F]/g, '') + points.join('\r\n') + '\r\n##END';
+  return `${header.replace(/[^\t\r\n\x20-\x7F]/g, '') + points.join('\r\n')}\r\n##END`;
 }
