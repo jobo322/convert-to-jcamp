f45ecea493b83becd42b11ddfdfc2b89c2b51360
Oceane Patiny <oceane@patiny.com>
2021-09-24 16:31:28 +0200

    fix: adapt getNumber to ts

--- src/utils/getNumber.ts
+++ src/utils/getNumber.ts
@@ -1,4 +1,4 @@
-export function getNumber(number, factor) {
+export function getNumber(number: number, factor: number) {
   if (factor !== 1) number /= factor;
   const rounded = Math.round(number);
   if (rounded !== number && Math.abs(rounded - number) <= Number.EPSILON) {
