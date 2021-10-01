3888c43c91e4d4bb9bbf10c86961ec445e8f6642
Oceane Patiny <oceane@patiny.com>
2021-10-01 12:01:38 +0200

    doc: add function doc

--- src/utils/getNumber.ts
+++ src/utils/getNumber.ts
@@ -1,4 +1,10 @@
-export function getNumber(number: number, factor: number) {
+/**
+ * Reconvert number to original value
+ * @param number Number used for computation
+ * @param factor Multiplying factor
+ * @returns Original value
+ */
+export function getNumber(number: number, factor: number):number {
   if (factor !== 1) number /= factor;
   const rounded = Math.round(number);
   if (rounded !== number && Math.abs(rounded - number) <= Number.EPSILON) {
