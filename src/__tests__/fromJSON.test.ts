bb310338952168f88eebad3ae80e094dd7c343c1
jobo322 <jose.bolanos@correounivalle.edu.co>
2021-11-07 16:24:47 +0100

    fix: xyDataCreator

---
+++ src/__tests__/fromJSON.test.ts
@@ -0,0 +1,20 @@
+// @ts-expect-error
+import { convert } from 'jcampconverter';
+
+import { fromJSON } from '../fromJSON';
+
+describe('fromJSON', () => {
+  it('create and read spectrum', () => {
+    const spectrum = {
+      x: [1, 2, 3, 4],
+      y: [2, 3, 4, 5],
+    };
+
+    const jcamp = fromJSON(spectrum, { xydata: true});
+    const spectrumReaded = convert(jcamp, { xy: true });
+    const spectrumData = spectrumReaded.flatten[0].spectra[0].data;
+
+    expect(spectrumData.x).toStrictEqual(spectrum.x);
+    expect(spectrumData.y).toStrictEqual(spectrum.y);
+  });
+});
