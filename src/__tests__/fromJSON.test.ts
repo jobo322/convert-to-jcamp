bcf5d3339188fc248c64cf2b2734e062902d94c3
jobo322 <jose.bolanos@correounivalle.edu.co>
2021-11-25 06:21:44 +0100

    chore: add test cases for data encoder

--- src/__tests__/fromJSON.test.ts
+++ src/__tests__/fromJSON.test.ts
@@ -3,17 +3,61 @@ import { convert } from 'jcampconverter';
 
 import { fromJSON } from '../fromJSON';
 
+const createData = (firstX: number, lastX: number, nbPoints: number) => {
+  const x = new Array(nbPoints);
+  const jump = (lastX - firstX) / (nbPoints - 1);
+  for (let i = 0; i < nbPoints; i++) {
+    x[i] = firstX + jump * i;
+  }
+  return x;
+};
+
+const xAxis = createData(1, 150, 150);
+const yPositive = createData(1, 150, 150);
+const yNegative = createData(-1, -150, 150);
+const yZeros = createData(0, 0, 150);
+
 describe('fromJSON', () => {
-  it('create and read spectrum', () => {
+  it('create and read spectrum differents encoding', () => {
     const spectrum = {
-      x: [1, 2, 3, 4],
-      y: [2, 3, 4, 5],
+      x: xAxis,
+      y: yPositive
     };
 
-    const jcamp = fromJSON(spectrum, { xydata: true });
+    const encodingList = ['DIFF', 'DIFDUP', 'FIX', 'SQZ', 'CSV', 'PAC'];
+    for (let encoding of encodingList) {
+      const jcamp = fromJSON(spectrum, { xydata: true, encoding });
+      const spectrumReaded = convert(jcamp, { xy: true });
+      const spectrumData = spectrumReaded.flatten[0].spectra[0].data;
+
+      expect(spectrumData.x).toStrictEqual(spectrum.x);
+      expect(spectrumData.y).toStrictEqual(spectrum.y);
+    }
+  });
+
+  it('create and read spectrum y in zeros differents encoding', () => {
+    const spectrum = {
+      x: xAxis,
+      y: yZeros
+    };
+
+    const encodingList = ['DIFF', 'DIFDUP', 'FIX', 'SQZ', 'CSV', 'PAC'];
+    for (let encoding of encodingList) {
+      const jcamp = fromJSON(spectrum, { xydata: true, encoding });
+      const spectrumReaded = convert(jcamp, { xy: true });
+      const spectrumData = spectrumReaded.flatten[0].spectra[0].data;
+
+      expect(spectrumData.x).toStrictEqual(spectrum.x);
+      expect(spectrumData.y).toStrictEqual(spectrum.y);
+    }
+  });
+
+  it('create and read spectrum DIFDUP encoding negative values', () => {
+    const spectrum = { x: xAxis, y: yNegative };
+
+    const jcamp = fromJSON(spectrum, { xydata: true, encoding: 'DIFDUP' });
     const spectrumReaded = convert(jcamp, { xy: true });
     const spectrumData = spectrumReaded.flatten[0].spectra[0].data;
-
     expect(spectrumData.x).toStrictEqual(spectrum.x);
     expect(spectrumData.y).toStrictEqual(spectrum.y);
   });
