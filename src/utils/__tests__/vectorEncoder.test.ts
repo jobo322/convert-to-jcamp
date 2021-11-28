1885a0b623641d619ff1f7ee2db28936bdf0ece1
Luc Patiny <luc@patiny.com>
2021-11-28 07:42:54 +0100

    test: add more DIFDUP tests

--- src/utils/__tests__/vectorEncoder.test.ts
+++ src/utils/__tests__/vectorEncoder.test.ts
@@ -11,6 +11,16 @@ describe('encoding', () => {
     const encodedData = encode(y, 0, 1, 'DIFDUP');
     expect(encodedData).toBe('0EjVM%TNj12J02\r\n10@');
   });
+  it('DIFDUP encoding publication example', () => {
+    const y = [1, 2, 3, 3, 2, 1, 0, -1, -2, -3];
+    const encodedData = encode(y, 0, 1, 'DIFDUP');
+    expect(encodedData).toBe('0AJT%jX');
+  });
+  it('DIFDUP encoding start 1000', () => {
+    const y = [1, 2, 3, 3, 2, 1, 0, -1, -2, -3];
+    const encodedData = encode(y, 1000, 1, 'DIFDUP');
+    expect(encodedData).toBe('1000AJT%jX');
+  });
   it('SQZ encoding', () => {
     const y = [5, 4, 3, 2, 1, 5, 5, 5, 10, -102, 0];
     const encodedData = encode(y, 0, 1, 'SQZ');
