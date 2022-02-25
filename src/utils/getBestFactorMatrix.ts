b14343765721f95dc2f29f5074b8fa5348f74020
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-02-25 12:10:30 +0100

    chore: add getBestFactorMatrix

---
+++ src/utils/getBestFactorMatrix.ts
@@ -0,0 +1,44 @@
+import { DoubleMatrix, matrixMinMaxZ } from 'ml-spectra-processing';
+
+import { getFactorNumber } from './getFactorNumber';
+
+interface MinMax {
+  min: number;
+  max: number;
+}
+export function getBestFactorMatrix(
+  matrix: DoubleMatrix,
+  options: {
+    factor?: number;
+    /**
+     * The maximum absolute value
+     */ maxValue?: number;
+    minMax?: MinMax;
+  } = {},
+): number {
+  const { maxValue, factor, minMax } = options;
+
+  if (factor !== undefined) {
+    return factor;
+  }
+
+  // is there non integer number ?
+  let onlyInteger = true;
+  for (let row of matrix) {
+    for (let y of row) {
+      if (Math.round(y) !== y) {
+        onlyInteger = false;
+        break;
+      }
+    }
+  }
+  if (onlyInteger) {
+    return 1;
+  }
+  // we need to rescale the values
+  // need to find the max and min values
+  const extremeValues = minMax || matrixMinMaxZ(matrix);
+  return getFactorNumber(extremeValues, maxValue);
+}
+
+
